d7d3b818aa3a91be7225899ab90460e6dd78426d
WenceslaoGoedelmann <wengoe@gmail.com>
2024-02-17 15:51:28 -0600

    corrijo turnos

--- controllers/turnos.ts
+++ controllers/turnos.ts
@@ -6,144 +6,159 @@ import Turno, { ITurno } from "../models/turnos";
 import { sendToAdminEmail } from "../mailer/mailer";
 import { IUser } from "../models/usuario";
 
-export const getUserTurnos =async (req:Request, res:Response): Promise<void> => {
+export const getUserTurnos = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const usuarioId: ObjectId = req.body.usuarioConfirmado._id; //esto viene de la validacion del JWT
 
-    const usuarioId: ObjectId = req.body.usuarioConfirmado._id;//esto viene de la validacion del JWT
+  const consulta = { user: usuarioId };
 
-    const consulta = {user: usuarioId}
+  const turnos = await Turno.find(consulta); //busco en la base de datos todas (ver que uso find y no findOne) las ordenes que tiene el usuario
 
-    const turnos = await Turno.find(consulta) //busco en la base de datos todas (ver que uso find y no findOne) las ordenes que tiene el usuario
+  res.json({
+    data: [...turnos],
+  });
+};
 
+export const createTurno = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const usuarioId: ObjectId = req.body.usuarioConfirmado._id;
+
+  const usuario: IUser = req.body.usuarioConfirmado;
+
+  const turnoData: ITurno = req.body;
+
+  const data = {
+    ...turnoData, //toda la data que viene del body
+    user: usuarioId, //le agremmos la info del usuario
+    nombre: usuario.nombre,
+    surname: usuario.surname,
+    age: usuario.age,
+    email: usuario.email,
+    cellphone: usuario.cellphone,
+    location: usuario.location,
+    address: usuario.address,
+    height: usuario.height,
+    weight: usuario.weight,
+    experience: usuario.experience,
+    createdAt: new Date(), //le agremmos la fecha de creacion
+    status: "Pendiente", //le agremmos el estado por defecto "Pendiente"
+  };
+
+  const turno = new Turno(data);
+
+  await turno.save();
+
+  await sendToAdminEmail(usuario, turnoData); //enviamos el email para verificar el usuario
+
+  res.status(201).json({
+    turno,
+  });
+};
+
+export const AdminGetTurnos = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const consulta = req.body;
+
+  delete consulta.usuarioConfirmado;
+
+  const turnos = await Turno.find(consulta);
+
+  res.json({
+    data: [...turnos],
+  });
+};
+
+export const getHours = async (req: Request, res: Response): Promise<void> => {
+  const date = req.body;
+  try {
+    const turnos = await Turno.find(date);
+    if (!turnos) {
+      //si no existe respondemos con el error
+      res.status(400).json({
+        msg: "no hay turnos reservados para ese dia",
+      });
+      return;
+    }
+
+    const hours = turnos.map((e) => e.hour);
     res.json({
-        data: [ ...turnos]
-    })
-}
+      data: [...hours],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Error en el servidor",
+    });
+  }
+};
 
-export const createTurno =async (req:Request, res:Response):Promise<void> => {
-    const usuarioId: ObjectId = req.body.usuarioConfirmado._id;
+export const statusTurno = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { _id, status } = req.body; //desestructuramos la respuesta que viene en el body de la Request
 
-    const usuario: IUser = req.body.usuarioConfirmado;
+  try {
+    const turno = await Turno.findOne({ _id });
 
-    const turnoData: ITurno = req.body;
-
-    const data = {
-        ...turnoData, //toda la data que viene del body
-        user: usuarioId, //le agremmos la info del usuario
-        nombre:usuario.nombre,
-        surname:usuario.surname,
-        email:usuario.email,
-        createdAt: new Date(), //le agremmos la fecha de creacion
-        status: "pending" //le agremmos el estado por defecto "pending"
+    if (!turno) {
+      //si no existe el usuario respondemos con el error
+      res.status(400).json({
+        msg: "No se encontró el turno en la base de datos",
+      });
+      return;
     }
 
-    const turno = new Turno(data);
+    //si esta todo OK, actualizo el usuario
+    const turnoActualizado = await Turno.findOneAndUpdate(
+      { _id },
+      { status: status }
+    );
+    //y respondo al front
+    res.status(200).json({
+      msg: "turno actualizado con éxito",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Error en el servidor",
+    });
+  }
+};
 
-    await turno.save()
+export const deleteTurno = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { _id } = req.body; //desestructuramos la respuesta que viene en el body de la Request
 
-    await sendToAdminEmail(usuario, turnoData) //enviamos el email para verificar el usuario
+  try {
+    const turno = await Turno.findOne({ _id });
 
-    res.status(201).json({
-        turno
-    })
-}
-
-
-
-export const AdminGetTurnos =async (req:Request, res:Response): Promise<void> => {
-
-      const consulta = req.body
-      
-      delete consulta.usuarioConfirmado
-   
-  
-    const turnos = await Turno.find(consulta) 
-
-    res.json({
-        data: [ ...turnos]
-    })
-}
-
-export const getHours =async (req:Request, res:Response): Promise<void> => {
-    const date = req.body;
-    try {
-        const turnos = await Turno.find(date) 
-        if(!turnos){ //si no existe respondemos con el error
-            res.status(400).json({
-                msg: "no hay turnos reservados para ese dia"
-            });
-            return
-        }
-
-        const hours =turnos.map(e => e.hour)
-        res.json({
-            data: [...hours]
-        })
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            msg: "Error en el servidor"
-        })
+    if (!turno) {
+      //si no existe el usuario respondemos con el error
+      res.status(400).json({
+        msg: "No se encontró el turno en la base de datos",
+      });
+      return;
     }
 
-   
-}
-
-export const statusTurno =async (req:Request, res:Response):Promise<void> => {
-    const { _id, status } = req.body //desestructuramos la respuesta que viene en el body de la Request
-    
-    try{
-        const turno = await Turno.findOne({_id}) 
-
-        if(!turno){ //si no existe el usuario respondemos con el error
-            res.status(400).json({
-                msg: "No se encontró el turno en la base de datos"
-            })
-            return
-        }
-
-        
-        //si esta todo OK, actualizo el usuario
-        const turnoActualizado = await Turno.findOneAndUpdate({_id},{status: status})
-        //y respondo al front
-        res.status(200).json({
-            msg: "turno actualizado con éxito"
-        })
-
-    }catch(error){
-        console.log(error)
-        res.status(500).json({
-            msg: "Error en el servidor"
-        })
-    }
-}
-
-export const deleteTurno =async (req:Request, res:Response):Promise<void> => {
-    const { _id } = req.body //desestructuramos la respuesta que viene en el body de la Request
-    
-    try{
-        const turno = await Turno.findOne({_id}) 
-
-        if(!turno){ //si no existe el usuario respondemos con el error
-            res.status(400).json({
-                msg: "No se encontró el turno en la base de datos"
-            })
-            return
-        }
-
-        
-        //si esta todo OK, actualizo el usuario
-        const turnoEliminado = await Turno.findOneAndDelete({_id})
-        //y respondo al front
-        res.status(200).json({
-            msg: "turno eliminado con éxito"
-        })
-
-    }catch(error){
-        console.log(error)
-        res.status(500).json({
-            msg: "Error en el servidor"
-        })
-    }
-}
-
+    //si esta todo OK, actualizo el usuario
+    const turnoEliminado = await Turno.findOneAndDelete({ _id });
+    //y respondo al front
+    res.status(200).json({
+      msg: "turno eliminado con éxito",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Error en el servidor",
+    });
+  }
+};
--- models/turnos.ts
+++ models/turnos.ts
@@ -3,50 +3,85 @@ import { Model, Schema, Types, model } from "mongoose";
 export interface ITurno {
   createdAt: Date;
   user: Types.ObjectId;
-  nombre:string;
-  surname:string;
-  email:string;
+  nombre: string;
+  surname: string;
+  age: string;
+  email: string;
+  cellphone: string;
+  location: string;
+  address: string;
+  height: string;
+  weight: string;
+  experience: string;
   status: string;
   date: string;
   hour: string;
 }
 
 const TurnoSchema = new Schema<ITurno>({
-    createdAt:{
-        type: Date,
-        default: Date.now
-    },
-    user:{
-        type: Schema.Types.ObjectId,
-        ref: "Usuario",
-        required: true
-    },
-    nombre: {
-        type: String,
-        required: true
-    },
-    surname: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: String,
-        required: true
-    },
-    hour: {
-        type: String,
-        required: true
-    },
-})
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "Usuario",
+    required: true,
+  },
+  nombre: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cellphone: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: String,
+    required: true,
+  },
+  weight: {
+    type: String,
+    required: true,
+  },
+  experience: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  hour: {
+    type: String,
+    required: true,
+  },
+});
 
-const Turno: Model<ITurno> = model<ITurno>("Turno", TurnoSchema)
+const Turno: Model<ITurno> = model<ITurno>("Turno", TurnoSchema);
 
-export default Turno
+export default Turno;
